c26868fa7dfdda05991ca2b45b73d730a90cf438
MgKyaw <mgkyaw20112@gmail.com>
2025-10-26 16:30:07 +0700

    feat: Create global readonly variable

--- FluentValidation.Demo.Test/WeatherForecastValidatorTests.cs
+++ FluentValidation.Demo.Test/WeatherForecastValidatorTests.cs
@@ -4,6 +4,8 @@ namespace FluentValidation.Demo.Test
 {
     public class WeatherForecastValidatorTests
     {
+        private readonly WeatherForecastValidator _validator = new WeatherForecastValidator();
+
         [Fact]
         public void Test1()
         {
